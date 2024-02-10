17cc648f608552b6db757c78786de94a696d7ae0
Sawant Kumar <kumar.sawant37@gmail.com>
2024-02-10 22:52:16 +0530

    minor changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,10 +11,10 @@ const Navbar = ({ isDarkMode, onSwitchChange }) => {
 
   return (
     <nav>
-      <div className={`flex items-center justify-between w-100 px-[50px] py-6 ${isDarkMode ? 'bg-black text-white' : 'bg-white text-black'}`}>
+      <div className={`flex items-center justify-between w-100 px-[50px] py-6 ${isDarkMode ? 'bg-[#0B1623] text-white' : 'bg-white text-black'}`}>
         <span className={`font-extrabold text-3xl`}>@MailMagnet</span>
         <div className='w-[350px] flex items-center justify-between'>
-          <Switch size='lg' colorScheme='cyan' onChange={handleSwitchChange} />
+         <Switch size='lg' colorScheme='cyan' onChange={handleSwitchChange} />
           <button className='font-bold text-xl'>Sign In</button>
           <button className={`font-bold text-xl bg-cyan-500 px-7 py-2 rounded-full ${isDarkMode ? ' text-black' : 'text-white'}`}>Sign Up</button>
         </div>
