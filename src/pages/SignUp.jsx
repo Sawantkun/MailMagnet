a443a7949f96381b4dcb2adab4905511b1626f54
Sawant Kumar <kumar.sawant37@gmail.com>
2024-02-20 12:34:21 +0530

    kjnfkjn3

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -54,10 +54,11 @@ const SignUp = () => {
                             </InputRightElement>
                         </InputGroup>
                     </Stack>
-                    <div className='text-center mt-9'>
-                        <button className="font-bold text-white text-xl bg-cyan-500 px-7 py-2 rounded-full">Save</button>
+                    <div className='w-100 text-center mt-5 cursor-pointer opacity-50 hover:opacity-100 transition-all'>Forgot Password?</div>
+                        <div className='text-center mt-5'>
+                                <button className={`font-bold text-md bg-cyan-500 px-5 py-1 rounded-full ${isDarkMode ? ' text-black' : 'text-white'}`}>Create</button>
+                        </div>
                     </div>
-                </div>
             <img src={Hero} className='w-[400px]  mx-auto my-7' alt="Hero Image" />
             </div>
         </div>
